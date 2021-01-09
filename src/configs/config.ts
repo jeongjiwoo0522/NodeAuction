6e4b443533f34884536205e7212acdc9114c514b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-01-09 21:12:53 +0900

    UPDATE database config

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.join(__dirname, "../.env") });
 
 const env: NodeJS.ProcessEnv = process.env;
 
--- src/configs/connectionOptions.ts
+++ src/configs/connectionOptions.ts
@@ -1,15 +1,17 @@
 import config from "./config";
 import { ConnectionOptions } from "typeorm";
+import path from "path";
 
 const connectionOptions: ConnectionOptions = {
   type: "mysql",
   host: config.dbHost,
   port: +config.dbPort,
-  username: config.dbName,
+  username: config.dbUser,
   password: config.dbPassword,
   database: config.dbName,
-  synchronize: false,
-  entities: ["./models/**/*.ts"],
+  synchronize: true,
+  logging: true,
+  entities: [path.join(__dirname, "../models/**/*.ts")],
 }
 
 export { connectionOptions };
